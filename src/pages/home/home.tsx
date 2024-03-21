dd921aea9e1dedca0a16b0877b51d974065b49dd
BlizzardBlast <fdsurya7@gmail.com>
2024-03-21 20:23:46 +0700

    fix: delete console log

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,10 +2,8 @@ import MetaTag from '@/components/metaTag/metaTag';
 import HomeThree from '@/pages/home/HomeThree.tsx';
 import HomeOne from '@/pages/home/homeOne.tsx';
 import HomeTwo from '@/pages/home/homeTwo.tsx';
-import { VITE_FETCH_URL } from '@/utils/urls.ts';
 
 export default function Home(): JSX.Element {
-  console.log(VITE_FETCH_URL);
   return (
     <>
       <MetaTag
