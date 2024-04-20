7125ef80af116869c24dc81ee817578696e155fc
BlizzardBlast <fdsurya7@gmail.com>
2024-04-20 21:21:07 +0700

    feat: added animation in home two

--- src/pages/home/home-two.tsx
+++ src/pages/home/home-two.tsx
@@ -3,11 +3,19 @@ import DrinkLady from '@/assets/ladywithadrink.svg';
 import LoadImage from '@/components/load-image/load-image';
 import HeadingOne from '@/components/typography/headingOne.tsx';
 import Paragraph from '@/components/typography/paragraph.tsx';
+import { motion } from 'framer-motion';
 
 export default function HomeTwo(): JSX.Element {
   return (
     <div className="flex min-h-[70vh] flex-col items-center justify-around gap-10 bg-[url('@/assets/coffee_beans_bg.svg')] bg-cover bg-center bg-no-repeat py-10 md:flex-row">
-      <div className='flex flex-[4] items-center justify-center ps-3'>
+      <motion.div
+        className='flex flex-[4] items-center justify-center ps-3'
+        initial={{ opacity: 0, x: -10 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true, amount: 0.3 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5, ease: 'easeInOut' }}
+      >
         <LoadImage
           source={CoffeeLady}
           alternative='Coffee_Lady'
@@ -22,8 +30,15 @@ export default function HomeTwo(): JSX.Element {
           classes='h-80 w-[10.21875rem]'
           divClasses='flex justify-center items-center'
         />
-      </div>
-      <div className='flex flex-[6] items-center justify-center'>
+      </motion.div>
+      <motion.div
+        className='flex flex-[6] items-center justify-center'
+        initial={{ opacity: 0, x: 10 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true, amount: 0.3 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5, ease: 'easeInOut' }}
+      >
         <article className='flex min-h-80 w-[80%] flex-col items-center justify-center text-pretty rounded-3xl bg-tertiary-color px-7 text-center'>
           <HeadingOne className='text-center'>
             Spice up your coffee break
@@ -33,7 +48,7 @@ export default function HomeTwo(): JSX.Element {
             eiusmod tempor incididunt minim veniam, quis nostrud exercitation
           </Paragraph>
         </article>
-      </div>
+      </motion.div>
     </div>
   );
 }
