163c2138d9d6c4ab33fa43e178800df5b8923536
BlizzardBlast <fdsurya7@gmail.com>
2024-05-23 16:08:18 +0700

    fix: fixed redirected refresh

--- src/hooks/useSignedInRoute.ts
+++ src/hooks/useSignedInRoute.ts
@@ -5,36 +5,38 @@ import { useLocation, useNavigate } from 'react-router-dom';
 export default function useSignedInRoute(): boolean {
   const navigate = useNavigate();
   const location = useLocation();
-  const { isSignedIn } = useUserContext();
+  const { isSignedIn, isPending } = useUserContext();
   const [isSignedInRouteValidated, setIsSignedInRouteValidated] =
     useState(false);
 
   useEffect(() => {
-    const privateRoutes = [
-      '/profile',
-      '/cart',
-      '/find-your-coffee',
-      '/find-your-coffee/result',
-      '/transaction-history',
-      '/modify-product',
-      '/add-product',
-      '/promo',
-      '/add-promo'
-    ];
-    const isPrivateRoute =
-      privateRoutes.includes(location.pathname) ||
-      location.pathname.startsWith('/transaction/') ||
-      location.pathname.startsWith('/edit-product/');
-    if (!isSignedIn && isPrivateRoute) {
-      navigate('/');
-    }
+    if (!isPending) {
+      const privateRoutes = [
+        '/profile',
+        '/cart',
+        '/find-your-coffee',
+        '/find-your-coffee/result',
+        '/transaction-history',
+        '/modify-product',
+        '/add-product',
+        '/promo',
+        '/add-promo'
+      ];
+      const isPrivateRoute =
+        privateRoutes.includes(location.pathname) ||
+        location.pathname.startsWith('/transaction/') ||
+        location.pathname.startsWith('/edit-product/');
+      if (!isSignedIn && isPrivateRoute) {
+        navigate('/');
+      }
 
-    const publicRoutes = ['/sign-in', '/sign-up'];
-    if (isSignedIn && publicRoutes.includes(location.pathname)) {
-      navigate('/');
+      const publicRoutes = ['/sign-in', '/sign-up'];
+      if (isSignedIn && publicRoutes.includes(location.pathname)) {
+        navigate('/');
+      }
+      setIsSignedInRouteValidated(true);
     }
-    setIsSignedInRouteValidated(true);
-  }, [isSignedIn, location.pathname, navigate]);
+  }, [isSignedIn, location.pathname, navigate, isPending]);
 
   return isSignedInRouteValidated;
 }
