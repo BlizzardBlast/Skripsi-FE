996119e6bb32cf21bf314091154d52f897d0972c
BlizzardBlast <fdsurya7@gmail.com>
2024-06-08 11:25:12 +0700

    fix: fixed roasting type in cart

--- src/pages/cart/cart-products-handler.tsx
+++ src/pages/cart/cart-products-handler.tsx
@@ -97,9 +97,9 @@ export default function CartProductsHandler({
         <SelectContent>
           <SelectGroup>
             <SelectLabel>Roasting Type</SelectLabel>
-            <SelectItem value='low'>Low</SelectItem>
+            <SelectItem value='light'>Light</SelectItem>
             <SelectItem value='medium'>Medium</SelectItem>
-            <SelectItem value='high'>High</SelectItem>
+            <SelectItem value='dark'>Dark</SelectItem>
           </SelectGroup>
         </SelectContent>
       </Select>
